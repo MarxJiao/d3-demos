6e77b6621cb8155d1ae0440bfaaaafb03c90b1c0
MarxJiao <marxjiao@gmail.com>
2017-04-02 17:43:49 +0800

    add dependencies

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+/**
+ * @file webpack配置文件
+ * @author Marx
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+    entry: {
+        app: './src/index/index.js'
+    },
+    output: {
+        path: __dirname + '/dist',
+        filename: 'index.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './src/index/index.html'
+    })],
+    devServer:{
+        inline:true,
+        contentBase: __dirname + "dist",
+        compress: true,
+        port: 9000
+    }
+};
+module.exports = config;
