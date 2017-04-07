3e3e2e1fbd08771266753b3438a9246d1cf77cd8
MarxJiao <marxjiao@gmail.com>
2017-04-07 12:07:45 +0800

    修改主页样式

--- src/index/index.js
+++ src/index/index.js
@@ -6,13 +6,15 @@ console.log(files);
 
 const pages = document.getElementById('pages');
 
-const ul = document.createElement('ul');
+const ul = document.createElement('div');
+ul.setAttribute('class', 'panel panel-default')
 
 for (let file of files) {
     if (file === 'index') break;
-    let li = document.createElement('li');
+    let li = document.createElement('div');
+    li.setAttribute('class', 'panel-body')
     console.log(li)
-    li.innerHTML = '<a href="' + file + '.html">' + file + '</a>';
+    li.innerHTML = '<a href="' + file + '.html" target="_blank">' + file + '</a>';
     ul.appendChild(li);
 }
 
