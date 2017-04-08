733ec901970bc5a2db34b95108d571d095b99356
MarxJiao <marxjiao@gmail.com>
2017-04-08 22:23:25 +0800

    画折线图

--- src/demos/line/index.js
+++ src/demos/line/index.js
@@ -3,6 +3,8 @@
  * @author Mrax
  */
 
+import * as d3 from 'd3';
+
 // 数据源
 const dataset = [
     {
@@ -44,3 +46,78 @@ const dataset = [
         ]
     }
 ]
+
+
+// svg尺寸
+const width = 500;
+const height = 500;
+
+
+// 边距
+const padding = {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+}
+
+// 画svg区域
+const svg = d3.select('body')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+// 计算最大值
+let gdpmax = 0;
+for (let i = 0, l = dataset.length; i < l; i++) {
+    let currentGdp = d3.max(dataset[i].gdp, d => d[1])
+    gdpmax = currentGdp > gdpmax ? currentGdp : gdpmax;
+}
+
+// x比例尺
+const xScale = d3.scaleLinear()
+    .domain([2000, 2013])
+    .range([0, width - padding.left - padding.right]);
+
+// y比例尺
+const yScale = d3.scaleLinear()
+    .domain([0, gdpmax * 1.1])
+    .range([height - padding.top - padding.bottom, 0]);
+
+// 直线生成器
+const linePath = d3.line()
+    .x(d => xScale(d[0]))
+    .y(d => yScale(d[1]));
+
+console.log(linePath);
+
+// 定义颜色
+const colors = [d3.rgb(0, 0, 255), d3.rgb(0, 255, 0)];
+
+// 添加路径
+svg.selectAll('path')
+    .data(dataset)
+    .enter()
+    .append('path')
+    .attr('transform', 'translate(' + padding.left + ', ' + padding.top + ')')
+    .attr('d', d => linePath(d.gdp))
+    .attr('fill', 'none')
+    .attr('stroke-width', 3)
+    .attr('stroke', (d, i) => colors[i]);
+
+// x轴
+const xAxis = d3.axisBottom(xScale)
+    .ticks(5)
+    .tickFormat(d3.format('d')); // 去掉年份中间的逗号
+
+const yAxis = d3.axisLeft(yScale);
+
+// 添加x轴
+svg.append('g')
+    .attr('transform', 'translate(' + padding.left + ', ' + (height - padding.bottom) + ')')
+    .call(xAxis);
+
+// 添加y轴
+svg.append('g')
+    .attr('transform', 'translate(' + padding.left + ', ' +  padding.top + ')')
+    .call(yAxis);
